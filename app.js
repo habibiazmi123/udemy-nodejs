d3611e309c73083a02dea4a9839bd9a0231a01f9
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2022-08-07 16:45:39 +0700

    fix connection

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
+  'mongodb+srv://azmi:teuingatuh@cluster0.jaefo.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
